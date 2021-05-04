a3f708edecdcb9a270c8f09d69fdcd61311445ec
seba3567 <sebastian26481@gmail.com>
2021-05-04 13:51:33 -0400

    dotOS: build with gapps

--- dot_evert.mk
+++ dot_evert.mk
@@ -22,6 +22,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_OPLAUNCHER := true
 #TARGET_INCLUDE_OP_FILE_MANAGER := true
 USE_GAPPS := true
+WITH_GAPPS += true
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)

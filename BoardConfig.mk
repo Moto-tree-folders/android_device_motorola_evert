PixelExperience-Devices/device_motorola_lake

lake: Make vendor board-configuration mandatory
Change-Id: I2cb9aba5f858c845399aa80afdbbf97edfd801dd

BoardConfig.mk
@@ -61,4 +61,4 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # inherit from the proprietary version
--include vendor/motorola/lake/BoardConfigVendor.mk
+include vendor/motorola/lake/BoardConfigVendor.mk

Moto-tree-folders/android_device_motorola_evert

BoardConfig.mk
@@ -44,4 +44,4 @@ TARGET_HAS_NO_WLAN_STATS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # inherit from the proprietary version
--include vendor/motorola/evert/BoardConfigVendor.mk
+include vendor/motorola/evert/BoardConfigVendor.mk
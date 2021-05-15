f46477f672cb1b0198749ef474c9e7701f9b6a9e
seba3567 <sebastian26481@gmail.com>
2021-05-15 15:55:52 -0400

    Revert "Revert "lake: Don't let builds complete without vendor tree""
    This reverts commit d3a8b602fcb774090f5038163b0d0956005b59dc.

--- device.mk
+++ device.mk
@@ -15,7 +15,11 @@
 #
 
 # Vendor blobs
+<<<<<<< HEAD
 $(call inherit-product-if-exists, vendor/motorola/evert/evert-vendor.mk)
+=======
+$(call inherit-product, vendor/motorola/lake/lake-vendor.mk)
+>>>>>>> f7f024e (lake: Don't let builds complete without vendor tree)
 
 # Properties
 -include $(LOCAL_PATH)/vendor_prop.mk
